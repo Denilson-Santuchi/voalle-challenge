dd7c10a3074ee74f051c2d10605efd69cdffb662
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2023-01-13 11:48:08 -0300

    feat: alphabet funcions implemented

---
+++ src/ConsoleDiamond/Alphabet.cs
@@ -0,0 +1,21 @@
+namespace DiamondConsole;
+
+public static class Alphabet
+{
+    private static readonly string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-8.0/ranges
+    public static string Slice(char letter)
+    {
+        var index = alphabet.IndexOf(letter);
+        var slicedAlpha = alphabet[..(index + 1)];
+
+        return slicedAlpha;
+    }
+    public static string Reverse(string alphabet)
+    {
+        char[] slicedAlpha = alphabet.ToCharArray();
+        Array.Reverse(slicedAlpha);
+        return new string(slicedAlpha);
+    }
+}
